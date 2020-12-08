46f4fe828ab6e867574fd2badd384ec67534f358
xxd <752747830@qq.com>
2020-12-08 20:03:35 +0800

    a

---
+++ 2-node.js
@@ -0,0 +1,3 @@
+//引入lodash包
+const _ = require('lodash');
+console.log(_.random(1,100));
---
+++ 3-noemon.js
@@ -0,0 +1,3 @@
+require('http').createServer((request,response)=>{
+    response.end('123231231');
+}).listen(80);
